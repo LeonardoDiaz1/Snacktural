0e3080ab0c85d0b8638d1663826da7e970137a0e
Leonardo Díaz <107229055+LeonardoDiaz1@users.noreply.github.com>
2023-07-24 20:38:48 -0600

    Se agregaron cambios

--- Js/Alert.js
+++ Js/Alert.js
@@ -1,6 +1,6 @@
 function modal() {
     Swal.fire({
-        html: "<h1 style='font-size: 3rem;' class='titulo-modal'>Snacktural</h1><span class='mas-vendidos'>Nustro Contacto</span><p><i class='ti ti-phone-call'></i> 01 800 024 0384</p><br/><span class='mas-vendidos'>Nuestras Redes Sociales</h3><br/><a style='font-size: 20px; color: #3b5998; cursor: pointer;' class='ti ti-brand-facebook-filled'></a><a style='font-size: 20px; color: #171515; cursor: pointer; pointer; margin: 8px;' class='ti ti-brand-github-filled'></a><a style='font-size: 20px; color: #00acee; cursor: pointer;' class='ti ti-brand-twitter-filled'></a>",
+        html: "<h1 style='font-size: 3rem;' class='titulo-modal'>Snacktural</h1><span class='mas-vendidos'>Nustro Contacto</span><br/><br/><p><i class='ti ti-phone-call'></i> 01 800 024 0384</p><br/><span class='mas-vendidos'>Nuestras Redes Sociales</h3><br/><br/><a style='font-size: 20px; color: #3b5998; cursor: pointer;' class='ti ti-brand-facebook-filled'></a><a style='font-size: 20px; color: #171515; cursor: pointer; pointer; margin: 8px;' class='ti ti-brand-github-filled'></a><a style='font-size: 20px; color: #00acee; cursor: pointer;' class='ti ti-brand-twitter-filled'></a>",
         showDenyButton: true,
         showConfirmButton: false,
         denyButtonText: `Cerrar`,
